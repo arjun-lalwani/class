242b75bb0cec59a21be62f147e7fa87e36ce66b3
arjun-lalwani <arjun11@uw.edu>
2018-02-13 16:50:53 -0800

    update

--- react/activity-1/js/index.js
+++ react/activity-1/js/index.js
@@ -8,9 +8,20 @@ const people = [
 ];
 
 // Class for an individual person
-
+class Person extends React.Component {
+    render() {
+        return <h1>Hello, my name is {this.props.name} and I am interested in {this.props.interest}</h1>
+    }
+}
 
 // Class to represent a group of people
+class People extends React.Component {
+    render() {
+        return <div>
+            <Person />
+        </div>
+    }
+}
 
 // Render your component in the `main` section
-
+ReactDOM.render(element, document.getElementById('root'));
